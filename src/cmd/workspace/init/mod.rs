5808f76e5e1bcc03fe3a7be8b3f36ee3faf5522e
tmorin <thibault@morin.io>
2023-04-22 12:55:34 +0200

    fix(workspace): prevent initialization of an initialized workspace

--- src/cmd/workspace/init/mod.rs
+++ src/cmd/workspace/init/mod.rs
@@ -20,6 +20,13 @@ pub fn execute_workspace_init(arg_matches: &ArgMatches) -> Result<()> {
     let cache_path = Path::new(config.cache_directory.as_str());
     let source_path = Path::new(config.source_directory.as_str());
     let manifest_path = source_path.join(&config.workspace_manifest);
+    // stop if manifest already exists
+    if manifest_path.exists() {
+        return Err(anyhow::Error::msg(format!(
+            "The manifest {} already exists",
+            manifest_path.to_str().unwrap(),
+        )))?;
+    }
     // create cache directory
     create_directory(cache_path)?;
     // create source directory
@@ -42,6 +49,7 @@ pub fn execute_workspace_init(arg_matches: &ArgMatches) -> Result<()> {
 #[cfg(test)]
 mod test {
     use crate::cli::build_cli;
+    use crate::constants::WORKSPACE_MANIFEST;
     use crate::utils::delete_file_or_directory;
 
     use super::*;
@@ -64,11 +72,49 @@ mod test {
                 .subcommand_matches("init")
                 .unwrap(),
         )
-            .unwrap();
+        .unwrap();
         let path_workspace_manifest =
             Path::new("target/tests/cmd/workspace/init/source/.pgen-workspace.yaml");
         assert!(path_workspace_manifest.exists());
         let path_cache_directory = Path::new("target/tests/cmd/workspace/init/cache");
         assert!(path_cache_directory.exists());
+    }
+
+    #[test]
+    fn test_init_when_manifest_already_exist() {
+        let source_path = Path::new("target/tests/cmd/workspace/init/source");
+        let manifest_path = source_path.join(WORKSPACE_MANIFEST);
+
+        delete_file_or_directory(source_path).unwrap();
+        create_parent_directory(&manifest_path).unwrap();
+
+        std::fs::OpenOptions::new()
+            .write(true)
+            .create(true)
+            .open(manifest_path)
+            .expect("Couldn't open file");
+
+        let arg_matches = build_cli().get_matches_from([
+            "plantuml-generator",
+            "-l=Debug",
+            "workspace",
+            "init",
+            "-s=target/tests/cmd/workspace/init/source",
+            "-C=target/tests/cmd/workspace/init/cache",
+        ]);
+        match execute_workspace_init(
+            arg_matches
+                .subcommand_matches("workspace")
+                .unwrap()
+                .subcommand_matches("init")
+                .unwrap(),
+        ) {
+            Ok(_) => {
+                panic!("should raised an error")
+            }
+            Err(e) => {
+                assert_eq!(e.to_string(), "The manifest target/tests/cmd/workspace/init/source/.pgen-workspace.yaml already exists")
+            }
+        };
     }
 }
